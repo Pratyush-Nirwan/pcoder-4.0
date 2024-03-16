cf6df544be6114119f8df298e1ee810022ae6eb9
Pratyush-Nirwan <pratyushnirwan123@gmail.com>
2024-03-16 18:56:57 +0530

    fixed manifest and archive links

--- src/components/others/Versions.js
+++ src/components/others/Versions.js
@@ -4,7 +4,7 @@ export const V1 = () => {
 
     return (
         <>
-            <iframe title="web-iframe" id='web-iframe' src="https://pcoder.me/pcoder-1.0/">
+            <iframe title="web-iframe" id='web-iframe' src="https://pratyush-nirwan.github.io/pcoder-1.0/">
             </iframe>
         </>
     )
@@ -13,7 +13,7 @@ export const V1 = () => {
 export const V2 = () => {
     return (
         <>
-            <iframe title="web-iframe" id='web-iframe' src="https://pcoder.me/pcoder-2.0/">
+            <iframe title="web-iframe" id='web-iframe' src="https://pratyush-nirwan.github.io/pcoder-2.0/">
             </iframe>
         </>
     )
@@ -22,7 +22,7 @@ export const V2 = () => {
 export const V3 = () => {
     return (
         <>
-            <iframe title="web-iframe" id='web-iframe' src="https://pcoder.me/pcoder-3.0/">
+            <iframe title="web-iframe" id='web-iframe' src="https://pratyush-nirwan.github.io/pcoder-2.0/">
             </iframe>
         </>
     )
